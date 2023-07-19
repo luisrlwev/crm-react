840100023ecb3849080e70ea5c30e14e7fed02d0
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-19 17:59:29 -0500

    Creamos las páginas y vimos que es el Outlet - clase 156

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>Inicio</h1>
-  }
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <Index/>
+      },
+      {
+        path: '/clientes/nuevo',
+        element: <NuevoCliente/>
+      }
+    ]
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>Clientes</div>
+  )
+}
+
+export default index
