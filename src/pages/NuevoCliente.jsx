02568f9b95d656cc139567a8aa5593138d4763c3
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-20 17:55:24 -0500

    Hemos creado el formulario para registrar nuevos clientes - clase 165

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
+import Formulario from "../components/Formulario"
 
 function NuevoCliente() {
+
+  const navigate = useNavigate()
+
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className='font-black text-4xl text-blue-900'>Nuevo cliente</h1>
+      <p className='mt-3'>Llena todos los campos para registrar un nuevo cliente</p>
+
+      <div className='flex justify-end'>
+        <button className='bg-blue-800 text-white px-3 py-1 font-bold uppercase' onClick={() => navigate(-1)}>
+          Volver
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+        <form>
+          <Formulario/>
+          <input type="submit" className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg rounded-md" value="Registrar nuevo cliente"/>
+        </form>
+      </div>
+    </>
   )
 }
 
