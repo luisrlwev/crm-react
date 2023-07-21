9d624919308e763ae873762e277b35d57e44e8dc
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-21 17:34:31 -0500

    Agregamos validacion al email - clase 169

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -7,10 +7,17 @@ export async function action({request}) {
 
   const datos = Object.fromEntries(formData)
 
+  const email = formData.get('email')
+
   // Validación
   const errores = []
   if(Object.values(datos).includes('')){
     errores.push('Todos los campos son obligatorios')
+  }
+
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+  if(!regex.test(email)){
+    errores.push('El email no es válido')
   }
 
   // Retornar datos si hay errores
