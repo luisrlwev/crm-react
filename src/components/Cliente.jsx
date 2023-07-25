eae236612a75adcb2ca2ccc9e148ed58864d7afd
Luis Angel Hernandez <rlwev@hotmail.com>
2023-07-25 16:12:36 -0500

    Agregamos los errores por si no existen clientes y recuperamos el id del cliente para poder editarlo - clase 175

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,5 +1,8 @@
+import { useNavigate } from "react-router-dom"
+
 function Cliente({cliente}) {
 
+    const navigate = useNavigate()
     const { nombre, empresa, email, telefono, id } = cliente
 
   return (
@@ -13,8 +16,13 @@ function Cliente({cliente}) {
             <p className="text-gray-600"><span className="text-gray-800 uppercase font-bold">Teléfono: </span>{telefono}</p>
         </td>
         <td className="p-6 flex gap-3">
-            <button type="button" className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs">Editar</button>
-            <button type="button" className="text-red-600 hover:text-red-700 uppercase font-bold text-xs">Eliminar</button>
+            <button type="button"
+            className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+            onClick={() => navigate(`/clientes/${id}/editar`) }
+            >Editar</button>
+            <button type="button"
+            className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+            >Eliminar</button>
         </td>
     </tr>
   )
